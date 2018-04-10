8e3e750707e01fa2370cdbe4ef51014e61d1f50c
Flavian Durigu <flavi2k@gmail.com>
2018-04-10 12:04:52 +0300

    html report added

--- CucumberBasic1/CucumberBasic1/src/test/java/cucumberTest/TestRunner.java
+++ CucumberBasic1/CucumberBasic1/src/test/java/cucumberTest/TestRunner.java
@@ -9,7 +9,9 @@ import cucumber.api.junit.Cucumber;
 	@RunWith(Cucumber.class)
 	@CucumberOptions(
 			features = "Feature",
-			glue={"stepDefinition"}
+			glue={"stepDefinition"},
+			plugin = { "pretty", "html:target/cucumber-reports"  },
+			monochrome = true
 			)
 	
 	
